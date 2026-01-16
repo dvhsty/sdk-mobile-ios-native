939b6bcdf076ae4cd946d119446fd5cd66628eea
Daniel VH <daniel.vargahali@strivacity.com>
2026-01-16 16:08:06 +0100

    chore(ci): Specify available build destination

--- .github/workflows/unused-variables.sh
+++ .github/workflows/unused-variables.sh
@@ -3,7 +3,7 @@ derived_data_path=.build/DerivedData
 
 # Build the SDK package using xcodebuild.
 xcodebuild build -scheme SdkMobileIOSNative -sdk "`xcrun --sdk iphonesimulator --show-sdk-path`" \
--destination "OS=18.4,name=iPhone 16" -derivedDataPath ${derived_data_path} clean build
+-destination "platform=iOS Simulator,arch=arm64,name=iPhone 16" -derivedDataPath ${derived_data_path} clean build
 
 # Run Periphery to scan for unused variables.
 periphery scan --skip-build --index-store-path $derived_data_path/Index.noindex/DataStore
